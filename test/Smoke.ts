df5218eca45993771f6ec37b5eae34c696399878
olegnat <natsevsky@gmail.com>
2022-06-30 15:38:33 +0200

    cleanup

--- test/Smoke.ts
+++ test/Smoke.ts
@@ -3,22 +3,27 @@ import chaiAsPromised from 'chai-as-promised'
 import { solidity } from 'ethereum-waffle'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { ethers } from 'hardhat'
+import { Hello } from "../typechain"
 
 const { expect } = chai
 chai.use(chaiAsPromised)
 chai.use(solidity)
 
-describe('TetuVaultAssetManager tests', function () {
+describe('Smoke tests', function () {
   let deployer: SignerWithAddress
+  let hello: Hello
 
   beforeEach(async function () {
     ;[deployer,] = await ethers.getSigners()
      console.log(`deployer: ${deployer.address}`)
+     const HelloFact = await ethers.getContractFactory("Hello")
+     hello = await HelloFact.deploy()
+
   })
 
   describe('General tests', function () {
     it('Smoke test', async function () {
-        console.log(`Hey!`)
+        expect(await hello.hello()).is.eq("Hello")
     })
 
   })
