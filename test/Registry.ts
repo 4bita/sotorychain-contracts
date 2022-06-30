18eb1c1fe07365ea98be1f47c0ea0b938e15cdb8
Alexey Karpenko <38530214+k-alexey@users.noreply.github.com>
2022-07-01 00:19:54 +0200

    Fix Registry tests

--- test/Registry.ts
+++ test/Registry.ts
@@ -26,65 +26,64 @@ describe("Registry tests", function () {
     )
   })
 
-  describe("General tests", function () {
-    it("Register Story", async function () {
-      expect(
-        await registry.registerStory({
-          profileId: BigNumber.from("0"),
-          pubId: BigNumber.from("0"),
-        })
-      ).emit(registry, "storyRegistered")
-    })
-
-    it("Get Story", async function () {
+  it("Register Story", async function () {
+    expect(
       await registry.registerStory({
-        profileId: BigNumber.from("1"),
-        pubId: BigNumber.from("2"),
+        profileId: BigNumber.from("0"),
+        pubId: BigNumber.from("0"),
       })
-      const story = await registry.getStory({
-        profileId: BigNumber.from("1"),
-        pubId: BigNumber.from("2"),
-      })
-      expect(story[0][0]).is.equal(BigNumber.from("1"))
-      expect(story[0][1]).is.equal(BigNumber.from("2"))
+    ).emit(registry, "storyRegistered")
+  })
+
+  it("Get Story", async function () {
+    await registry.registerStory({
+      profileId: BigNumber.from("1"),
+      pubId: BigNumber.from("2"),
     })
-
-    it("Add candidate", async function () {
-      const head = {
-        profileId: BigNumber.from("1"),
-        pubId: BigNumber.from("2"),
-      }
-
-      const item1 = {
-        profileId: BigNumber.from("1"),
-        pubId: BigNumber.from("3"),
-      }
-
-      const item2 = {
-        profileId: BigNumber.from("1"),
-        pubId: BigNumber.from("3"),
-      }
-
-      await registry.registerStory(head)
-      await registry.appendStoryItemCandidate(head, 0, item1)
-      await registry.appendStoryItemCandidate(head, 0, item2)
-
-      const candidates = await registry.listStoryItemCandidates(
-        head,
-        BigNumber.from(0)
-      )
-
-      expect(candidates.length).is.equal(2)
-
-    it("Check allStories and getStoryByHash", async function () {
-      await registry.registerStory({
-        profileId: BigNumber.from("1"),
-        pubId: BigNumber.from("2"),
-      })
-      const _hash = await registry.allStories(0)
-      const story = await registry.getStoryByHash(_hash)
-      expect(story[0][0]).is.equal(BigNumber.from("1"))
-      expect(story[0][1]).is.equal(BigNumber.from("2"))
+    const story = await registry.getStory({
+      profileId: BigNumber.from("1"),
+      pubId: BigNumber.from("2"),
     })
+    expect(story[0][0]).is.equal(BigNumber.from("1"))
+    expect(story[0][1]).is.equal(BigNumber.from("2"))
+  })
+
+  it("Add candidate", async function () {
+    const head = {
+      profileId: BigNumber.from("1"),
+      pubId: BigNumber.from("2"),
+    }
+
+    const item1 = {
+      profileId: BigNumber.from("1"),
+      pubId: BigNumber.from("3"),
+    }
+
+    const item2 = {
+      profileId: BigNumber.from("1"),
+      pubId: BigNumber.from("3"),
+    }
+
+    await registry.registerStory(head)
+    await registry.appendStoryItemCandidate(head, 0, item1)
+    await registry.appendStoryItemCandidate(head, 0, item2)
+
+    const candidates = await registry.listStoryItemCandidates(
+      head,
+      BigNumber.from(0)
+    )
+
+    expect(candidates.length).is.equal(2)
+  })
+
+  it("Check allStories and getStoryByHash", async function () {
+    await registry.registerStory({
+      profileId: BigNumber.from("1"),
+      pubId: BigNumber.from("2"),
+    })
+    const _hash = await registry.allStories(0)
+    const story = await registry.getStoryByHash(_hash)
+    expect(story[0][0]).is.equal(BigNumber.from("1"))
+    expect(story[0][1]).is.equal(BigNumber.from("2"))
   })
 })
